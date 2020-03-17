bf3eb3d35ca24776885fe3277c0520e4858284d4
casey <nagasawa311@gmail.com>
2020-03-17 17:12:41 +0900

    asdfas

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ def meigen():
 
 	return message
 
-line_bot_api = LineBotApi(ENV['CHANNEL_ACCESS_TOKEN'])
+line_bot_api = LineBotApi("aSIw31tl8kO9bYUdBK4JPO/jFJ9JBC5Sr2lJo8/68QZ9R+CYuh7nrIZ8Rgby6h2Jerz7wOQw0v1tJM4ao2zRNc9ZPVZ3TkTcVHXkpA4Smc1H3SZY1Ge5qvTPShO+0LnUfRkUH9ojBQdH7riyGK5C4AdB04t89/1O/w1cDnyilFU=")
 
-line_bot_api.push_message(ENV['USER_ID'], TextSendMessage(text=meigen()))
+line_bot_api.push_message("U4d27304dab2cef4f34c16cf0be35fba1", TextSendMessage(text=meigen()))
 
 
 
