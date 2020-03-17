f459670779976dce579ae7a289f0fd3af4aaea4e
casey <nagasawa311@gmail.com>
2020-03-17 19:20:19 +0900

    environ

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from linebot.models import TextSendMessage
 import requests
 from bs4 import BeautifulSoup
 import random
+import os
 
 def meigen():
 	collect=[]
@@ -38,9 +39,9 @@ def meigen():
 
 	return message
 
-line_bot_api = LineBotApi("aSIw31tl8kO9bYUdBK4JPO/jFJ9JBC5Sr2lJo8/68QZ9R+CYuh7nrIZ8Rgby6h2Jerz7wOQw0v1tJM4ao2zRNc9ZPVZ3TkTcVHXkpA4Smc1H3SZY1Ge5qvTPShO+0LnUfRkUH9ojBQdH7riyGK5C4AdB04t89/1O/w1cDnyilFU=")
+line_bot_api = LineBotApi(os.environ["CHANNEL_ACCESS_TOKEN"])
 
-line_bot_api.push_message("U4d27304dab2cef4f34c16cf0be35fba1", TextSendMessage(text=meigen()))
+line_bot_api.push_message(os.environ["USER_ID"], TextSendMessage(text=meigen()))
 
 
 
